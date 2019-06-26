27a4b17cefb3fb15dc45a7f1b249181e677ecb22
valeriitovstyk <valeriitovstyk@gmail.com>
2019-06-26 21:30:38 +0300

    solution for statis posts by@laverka

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {users} from "./data/users";
+import {posts} from "./data/posts";
+import {comments} from "./data/comments";
+import PostList from './components/PostList'
 
 function App() {
+  const usersMap = users.reduce((acc, user) => ({ ...acc, [user.id]: user }), {});
+
+  const commentsMap = comments.reduce((acc, comment) => {
+    acc[comment.postId] = [...acc[comment.postId] || [], comment];
+    return acc;
+  }, {});
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PostList
+        users={usersMap}
+        posts={posts}
+        comments={commentsMap}
+    />
   );
 }
 
